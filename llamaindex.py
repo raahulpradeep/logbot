b776189ca732d492cf7b96f663a95b9ecc6fbc7d
raahulpradeep <rahulp0491@gmail.com>
2023-12-18 16:34:59 +0530

    Added prompts

--- llamaindex.py
+++ llamaindex.py
@@ -1,9 +1,24 @@
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
+import logging
+import sys
+import cmd
 
-documents = SimpleDirectoryReader("comp-data").load_data()
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+documents = SimpleDirectoryReader("logs").load_data()
 index = VectorStoreIndex.from_documents(documents)
-
 query_engine = index.as_query_engine()
-response = query_engine.query("What is the total compensation in 2019?")
-print(response)
 
+class Prompt(cmd.Cmd):
+    intro = 'Welcome to the Prompt. Type help or ? to list commands.\n'
+    prompt = '(logbot) '
+
+    # Command definitions
+    def do_query(self, arg):
+        """Query logs."""
+        response = query_engine.query(arg)
+        print(response)
+
+if __name__ == '__main__':
+    Prompt().cmdloop()
